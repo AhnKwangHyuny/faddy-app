d83036828614a9ccd18839248fba9ae4fbc79cfe
Ahn Kwang Hyun <ahnkwanghyun@Ahns-MacBook-Air.local>
2025-02-27 01:51:41 +0900

    첫 프로젝트 연결: 깃허브 저장소 초기 설정

---
+++ backend/src/main/java/faddy/FaddyApplication.java
@@ -0,0 +1,13 @@
+package faddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaddyApplication.class, args);
+	}
+
+}
